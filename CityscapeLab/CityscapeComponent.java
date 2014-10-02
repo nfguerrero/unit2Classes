24739eec6ba64b243da379e558c2539646afca5e
Nicolas Guerrero <nicg987@gmail.com>
2014-10-02 11:31:47 -0500

    Citscape start
    start of Cityscape lab

--- CityscapeLab/CityscapeComponent.java
+++ CityscapeLab/CityscapeComponent.java
@@ -6,28 +6,21 @@ import javax.swing.JComponent;
  * Class that creates instances of the classes that comprise the cityscape and delegates drawing the
  *  cityscape to these object.
  * 
- * @author @gcschmit
- * @version 18 July 2014
+ * @author nfguerrero
+ * @version 10/1/2014
  */
 public class CityscapeComponent extends JComponent
 {
     /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
+     *Default constructor for objects of class CityscapeComponent
      */
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
         
-        // create instances of classes and invoke the draw method on each
-        // ...
+        Road road = new Road();
+        
+        
         
         
     }
--- CityscapeLab/CityscapeViewer.java
+++ CityscapeLab/CityscapeViewer.java
@@ -3,8 +3,8 @@ import javax.swing.JFrame;
 /**
  * Class that contains the main method for the program and creates the frame containing the component.
  * 
- * @author @gcschmit
- * @version 18 July 2014
+ * @author nfguerrero
+ * @version 10/1/2014
  */
 public class CityscapeViewer
 {
@@ -14,19 +14,15 @@ public class CityscapeViewer
      */
     public static void main(String[] args)
     {
-        // create and configure the frame (window) for the program
         JFrame frame = new JFrame();
         
-        frame.setSize(800 /* x */, 600 /* y */);
-        frame.setTitle("Cityscape");
+        frame.setSize(800 , 600);
+        frame.setTitle("Zombie Apocolypse!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        // a frame contains a single component; create the Cityscape component and add it to the frame
         CityscapeComponent component = new CityscapeComponent();
         frame.add(component);
-        
-        // make the frame visible which will result in the paintComponent method being invoked on the
-        //  component.
+
         frame.setVisible(true);
     }
 
